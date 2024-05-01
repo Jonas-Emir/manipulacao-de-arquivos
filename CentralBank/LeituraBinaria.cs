51dbd6f3993fd47179e51ee53710dbd2edb8b1f3
Jonas Emir <jonasemir00@gmail.com>
2024-04-30 22:29:23 -0300

    Implementação para Leitura e Escrita Binaria

---
+++ CentralBank/LeituraBinaria.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralBank
+{
+    internal class LeituraBinaria
+    {
+        static void EscritaBinaria()
+        {
+            using (var fs = new FileStream("contaCorrente.txt", FileMode.Create))
+            using (var escritor = new BinaryWriter(fs))
+            {
+                escritor.Write(456);    // Número da agência
+                escritor.Write(546544);  // Número da conta
+                escritor.Write(4000.50);  // Saldo
+                escritor.Write("Elias Moreira");
+            }
+        }
+
+        static void LerBinario()
+        {
+            using (var fs = new FileStream("contaCorrente.txt", FileMode.Open))
+            using (var leitor = new BinaryReader(fs))
+            {
+                var agencia = leitor.ReadInt32();
+                var numeroConta = leitor.ReadInt32();
+                var saldo = leitor.ReadDouble();
+                var titular = leitor.ReadString();
+
+                Console.WriteLine($"{agencia}/{numeroConta} {titular} {saldo:F2}");
+            }
+        }
+
+        static void UsarStreamDeEntrada()
+        {
+            using (var fluxoDeEntrada = Console.OpenStandardInput())
+            using (var fs = new FileStream("entradaConsole.txt", FileMode.Create))
+            {
+                var buffer = new byte[1024]; // 1kb
+
+                while (true)
+                {
+                    var bytesLidos = fluxoDeEntrada.Read(buffer, 0, 1024);
+                    fs.Write(buffer, 0, bytesLidos);
+                    fs.Flush();
+                    Console.WriteLine($"Bytes lidos na console: {bytesLidos}");
+                }
+            }
+        }
+    }
+}
